61fd162146c6a7207df617efb165ddc52c53d1bc
Todd Kronenberg <toddkronenberg@gmail.com>
2014-11-22 09:17:57 -0800

    Setup Registration and Login with the database

--- public/js/script.js
+++ public/js/script.js
@@ -1,62 +1,72 @@
 var heiferApp = angular.module('heiferHub', ['ngRoute']);
+Parse.initialize("VLocePH0LazVLJVwfucZBRmihvfPiIITR6is2EVD", "Ir6EOlcEXSPlsrpMbdhpapJaPvpFwAiQncwSmkC7");
 
-// configure our routes
-heiferApp.config(function($routeProvider) {
-  $routeProvider
 
-  // route for the home page
-  .when('/', {
-    templateUrl : 'templates/login.html',
-    controller  : 'mainController'
-  })
 
-  // route for the about page
-  .when('/registration', {
-    templateUrl : 'templates/registration.html',
-    controller  : 'registrationController'
-  })
+// Angular Controller for Registration of new User
+heiferApp.controller('registrationController', function($scope) {
+    $scope.submit = function() {
+        var Users = Parse.Object.extend("Users");
+        var user = new Users();
+
+        // make sure password and confirm password match
+        if ($scope.passwd != $scope.confirmPasswd) {
+            alert("passwords don't match!");
+            return;
+        }
+
+        // stupid way to get expertise in a good array because forEach wasn't working
+        var expertise = $scope.expertise.split(",");
+        for (var i=0; i<expertise.length; i++)
+            expertise[i] = expertise[i].trim();
+
+        user.set("Name", $scope.fullName);
+        user.set("Password", $scope.passwd);
+        user.set("Office", $scope.cityCountry);
+        user.set("Position", $scope.position);
+        user.set("Expertise", expertise);
+        user.set("Picture", $scope.picture);
+        user.set("Phone", $scope.phone);
+        user.set("Email", $scope.email);
+
+        user.save(null, {
+            success: function(user) {
+                console.log("\n\n\nuser saved in\n\n\n");
+            },
+            error: function(user, error) {
+                console.log("\n\n\nfailed to create new object\n\n\n");
+            }
+        })
+
+    };
 });
 
 
-  // should save login info to parse database
-  heiferApp.controller('testMongo', function($scope) {
-    $scope.submit = function() {
-      // function refreshParse() {
-      Parse.initialize("VLocePH0LazVLJVwfucZBRmihvfPiIITR6is2EVD", "Ir6EOlcEXSPlsrpMbdhpapJaPvpFwAiQncwSmkC7");
 
-      var Users = Parse.Object.extend("Users");
-      var user = new Users();
+  // // should save login info to parse database
+  // heiferApp.controller('testMongo', function($scope) {
+  //   $scope.submit = function() {
+  //     // function refreshParse() {
+  //     Parse.initialize("VLocePH0LazVLJVwfucZBRmihvfPiIITR6is2EVD", "Ir6EOlcEXSPlsrpMbdhpapJaPvpFwAiQncwSmkC7");
 
-      console.log("email: " + $scope.email);
-      console.log("passwd: " + $scope.passwd);
-      user.set("Email", $scope.email);
-      user.set("Password", $scope.passwd);
+  //     var Users = Parse.Object.extend("Users");
+  //     var user = new Users();
 
-      user.save(null, {
-        success: function(user) {
-          console.log("user was saved");
-        },
-        error: function(user, error) {
-          console.log("failed to create new object");
-        }
-      });
+  //     console.log("email: " + $scope.email);
+  //     console.log("passwd: " + $scope.passwd);
+  //     user.set("Email", $scope.email);
+  //     user.set("Password", $scope.passwd);
 
-      // var query = new Parse.Query(Users);
-      // query.find({
-      //     success: function(results) {
-      //         var challenge = results.map(function(challenge) {
-      //             challenge.attributes.id = challenge.id;
-      //             return challenge.attributes;
-      //         });
-      //         obj={challenge: challenge};
-      //         refreshChallenge( { challenge: challenge } );
-      //     },
-      //     error: function(error) {
-      //         alert("Error: " + error.code + " " + error.message);
-      //     }
-      // });
-    };
-  });
+  //     user.save(null, {
+  //       success: function(user) {
+  //         console.log("user was saved");
+  //       },
+  //       error: function(user, error) {
+  //         console.log("failed to create new object");
+  //       }
+  //     });
+  //   };
+  // });
 
     // configure our routes
     heiferApp.config(function($routeProvider) {
@@ -90,7 +100,29 @@ heiferApp.config(function($routeProvider) {
     // create the controller and inject Angular's $scope
     heiferApp.controller('mainController', function($scope) {
         // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
+        // $scope.message = 'Everyone come and see how good I look!';
+        $scope.submit = function() {
+            var Users = Parse.Object.extend("Users");
+            // var user = new Users();
+            var query = new Parse.Query(Users);
+            console.log($scope.email);
+            console.log($scope.passwd);
+            query.equalTo("Email", $scope.email);
+            query.equalTo("Password", $scope.passwd);
+            query.find({
+                success: function(results) {
+                    // Parse isn't doing its job and always returns true
+                    // even with no matching results, so to handle login
+                    // failures vs. success i check results.length here:
+                    if (results.length > 0) {
+                        location.href="/#/feed";
+                    } else { console.log("jams failed"); }
+                },
+                error: function(error) {
+                    alert("Error: " + error.code + " " + error.message);
+                }
+            });
+        };
     });
 
     heiferApp.controller('aboutController', function($scope) {
